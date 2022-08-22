9c20927bbb6c36f60ccadc30af6a0726e8f16bc2
Sam Hilliard <samhilliard51@gmail.com>
2022-08-22 16:56:36 -0700

    grid column layout for contact form

--- site/src/components/Contact/Contact.js
+++ site/src/components/Contact/Contact.js
@@ -2,17 +2,23 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <section>
+    <section id="contact">
         <h2>Let's Get in Touch</h2>
-        <form>
-            <label for="name">Name</label>
-            <input type="text" id="name" name="name" />
+        <form id="contact-form">
+            <div className="form-input">
+              <label for="name">Name</label>
+              <input type="text" id="name" name="name" />
+            </div>
 
-            <label for="email">Email</label>
-            <input type="email" id="email" name="name" />
+            <div className="form-input">
+              <label for="email">Email</label>
+              <input type="email" id="email" name="name" />
+            </div>
 
-            <label for="message">Message</label>
-            <textarea id="message" name="message" rows="4" cols="50"></textarea>
+            <div className="form-input">
+              <label for="message">Message</label>
+              <textarea id="message" name="message" rows="4"></textarea>
+            </div>
 
             <input type="submit" value="Submit" />
         </form>
