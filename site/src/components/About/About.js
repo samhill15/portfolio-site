9cb6b3cc15c7351f60220421c298945aa3e25bc5
Sam Hilliard <samhilliard51@gmail.com>
2022-08-22 16:47:05 -0700

    split layout for about section and skill cards

--- site/src/components/About/About.js
+++ site/src/components/About/About.js
@@ -6,13 +6,17 @@ export default function About() {
   return (
     <section id="#about">
         <h2>About Me</h2>
-        <p>
-            I'm a Computer Science Undergrad at SDSU who loves
-            to learn about the web and computer security. In my
-            my free time, you can find me surfing, working out,
-            or building fun projects.
-        </p>
-        <img src={profilePic} height="300px" alt="This is me." />
+        <div className="split">
+          <div className="my-description">
+            <p>
+                I'm a Computer Science Undergrad at SDSU who loves
+                to learn about the web and computer security. In my
+                my free time, you can find me surfing, working out,
+                or building fun projects.
+            </p>
+          </div>
+          <img className="profile-pic" src={profilePic} alt="This is me." />
+        </div>
     </section>
   )
 }
