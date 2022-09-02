effd822ed9eb6257eb542637e412c506d142bb5c
Sam Hilliard <samhilliard51@gmail.com>
2022-09-01 19:17:16 -0700

    added type scaling

--- site/src/components/App/App.js
+++ site/src/components/App/App.js
@@ -3,7 +3,7 @@ import NavBar from '../NavBar/NavBar';
 import Header from '../Sections/Header/Header';
 import About from '../Sections/About/About';
 import Skills from '../Sections/Skills/Skills';
-import Projects from '../Projects/Projects';
+import Projects from '../Sections/Projects/Projects';
 import Contact from '../Sections/Contact/Contact';
 import Footer from '../Sections/Footer/Footer';
 
--- site/src/components/Sections/Projects/Projects.js
+++ site/src/components/Sections/Projects/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ProjectCard from '../ProjectCard/ProjectCard'
+import ProjectCard from '../../ProjectCard/ProjectCard'
 
 export default function Projects() {
   return (
