813bc24c8414437d6c944764535f6b5a8354bcaa
Sam Hilliard <samhilliard51@gmail.com>
2022-09-10 00:59:24 -0700

    fixed header layout

--- site/src/components/Sections/Header/Header.js
+++ site/src/components/Sections/Header/Header.js
@@ -3,15 +3,13 @@ import React from 'react'
 export default function Header() {
   return (
     <header>
-      <div className="container header-container">
-        <div className="header-titles">
-          <h4>I'm Sam Hillard.</h4>
-          <h1>Web Developer</h1>
-          <h2>Security Enthusiast</h2>
-        </div>
-        <div>
+      <div className="container centered header-container">
+          <div className="header-titles">
+            <h4>I'm Sam Hillard.</h4>
+            <h1>Web Developer</h1>
+            <h2>Security Enthusiast</h2>
+          </div>
           <a className="btn header-btn" href="#contact">Message Me</a>
-        </div>
       </div>
     </header>
   )
