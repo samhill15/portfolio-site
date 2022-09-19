1b936cfd51f44213bc7ecaba0394e07d02931e3a
Sam Hilliard <samhilliard51@gmail.com>
2022-09-18 23:14:53 -0700

    added more detail to skills section

--- site/src/components/Sections/Skills/Skills.js
+++ site/src/components/Sections/Skills/Skills.js
@@ -9,39 +9,38 @@ export default function Skills() {
           <div className="skill-cards-container">
             <SkillCard 
               name="Web Development"
-              details={<>HTML &#9679; CSS &#9679; Javascript 
-              &#9679; React &#9679; Node &#9679; 
-              Express &#9679; SQL &#9679; PHP &#9679; 
-              MongoDB &#9679; Responsive Design &#9679; 
-              RESTful APIs &#9679; JSON</>}
+              details={<>Design, Develop, and Deploy fast 
+              and responsive full stack web applications.</>}
             />
 
             <SkillCard
               name="Security"
-              details={<>Burpsuite &#9679; Zap Proxy &#9679; 
-              Metasploit &#9679; Linux Administration &#9679; 
-              Web App Penetration Testing &#9679; Shell 
-              Scripting</>}
+              details={<>Web application penetration testing, 
+              systems administration, and a variety of offensive strategies learned in CTFs.</>}
             />
 
             <SkillCard
               name="Programming"
-              details={<>Java &#9679; Python &#9679; Git 
-              &#9679; GitHub &#9679; Agile Development</>}
+              details={<>Knowledge of multiple programming language, 
+              object oriented concepts, and collaborating in an enterprise environment.</>}
             />
           </div>
           <div className="bio skills-bio">
             <p>
               My main area of expertise is developing fast and
               responsive web applications using modern technologies
-              such as Node, React, and MongoDB. 
+              such as Node, React, and MongoDB.
             </p>
             <p>
               I am also very passionate about learning cybersecurity, 
               so I've gained considerable knowledge
               about web application and general penetration testing by participating
               in clubs and competitions through school, and just by learning on
-              my own.
+              my own. One of the competitions I particpated included 
+              the <a href="https://nationalcyberleague.org/">National Cyber League</a>, which is
+              Capture the Flag oriented and taught me a variety of offensive techniques some of which include
+              cryptography and password cracking. I also competed in the <a href="https://www.nationalccdc.org/index.php/">National Collegiate Cyber Defense Competition</a> in which
+              I worked on a team to defend a virtual enterprise network from attackers. 
             </p>
             <p>
               Working as a Quality Assurance Software Engineer Intern
