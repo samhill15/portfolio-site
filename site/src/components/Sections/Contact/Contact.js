c0ac2a2c363be028748f22e76debb6cab07a2392
Sam Hilliard <samhilliard51@gmail.com>
2022-09-23 01:13:33 -0700

    added form input validation

--- site/src/components/Sections/Contact/Contact.js
+++ site/src/components/Sections/Contact/Contact.js
@@ -21,12 +21,49 @@ export default function Contact() {
   });
 
   const handleSubmit = e => {
-    console.log(inputs);
+
+    let inputError = false;
 
     // Error handling
-    // for (let key in inputs) {
-    //   console.log(`${key}: ${inputs[key].val}`);
-    // }
+    for (let key in inputs) {
+
+      if (inputs[key].val.length === 0) {
+        inputError = true;
+
+        setInputs(prevVals => {
+          return {
+            ...prevVals,
+            [key]: {
+              ...prevVals[key],
+              error: true
+            }
+          }
+        });
+
+      }
+    }
+
+    // send email and clear fields
+    if (!inputError) {
+      console.log("Ready to send an email!");
+
+      // setting all of the input vals to empty
+      // setting all of the error flags to false
+      for (let key in inputs) {
+        setInputs(prevVals => {
+          return {
+            ...prevVals,
+            [key]: {
+              val: '',
+              error: false
+            }
+          }
+        });
+      }
+    } else {
+      console.log("Errors Exist!");
+      console.log(inputs);
+    }
   
     e.preventDefault();
   }
@@ -50,19 +87,39 @@ export default function Contact() {
     <section id="contact">
         <h2>Let's Get in Touch</h2>
         <form onSubmit={handleSubmit} id="contact-form">
-            <div className="form-input-container name-container">
+            <div className={`form-input-container name-container ${inputs.name.error ? 'form-input-error': ''}`}>
               <label htmlFor="name">Name</label>
-              <input onChange={handleChange} className="form-input" type="text" id="name" name="name" />
+              <input 
+                onChange={handleChange} 
+                className="form-input" 
+                type="text" 
+                id="name" 
+                name="name" 
+                value={inputs.name.val} 
+              />
             </div>
 
-            <div className="form-input-container email-container">
+            <div className={`form-input-container email-container ${inputs.email.error ? 'form-input-error': ''}`}>
               <label htmlFor="email">Email</label>
-              <input onChange={handleChange} className="form-input" id="email" name="email" />
+              <input onChange={handleChange} 
+                className="form-input"
+                id="email" 
+                name="email"
+                value={inputs.email.val} 
+              />
             </div>
 
-            <div className="form-input-container message-container">
+            <div className={`form-input-container message-container ${inputs.message.error ? 'form-input-error': ''}`}>
               <label htmlFor="message">Message</label>
-              <textarea onChange={handleChange} className="form-input" id="message" name="message" rows="4"></textarea>
+              <textarea 
+                onChange={handleChange} 
+                className="form-input" 
+                id="message" 
+                name="message" 
+                rows="4"
+                value={inputs.message.val}
+              >
+              </textarea>
             </div>
 
             <div className="submit-btn-container">
