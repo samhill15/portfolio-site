09af36ff210e73aa693bd5f48059a99bfabfa738
Sam Hilliard <samhilliard51@gmail.com>
2022-10-11 21:01:22 -0700

    fixed container classname issue

--- client/src/components/Pages/Article/Article.js
+++ client/src/components/Pages/Article/Article.js
@@ -13,7 +13,7 @@ export default function Article() {
   })
 
   return (
-    <div className="containe">
+    <div className="container">
       <h1>{post.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
     </div>
